74c9817105989d69cd382b81232fd67ae63e5f8a
byKriz <cristianesquivelh@hotmail.com>
2022-08-17 15:57:54 -0400

    agregando tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import './index.css'
+
+
 
 function App() {
-
   return (
     <div className="App">
-      
+      <h1 className=" text-center font-bold text-4xl text-zinc-600">Holo world!</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
