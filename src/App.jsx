32a6f8d4da5e3a59e3a749830c50246e471f9991
byKriz <cristianesquivelh@hotmail.com>
2022-08-18 12:57:14 -0400

    probando la instalacion de tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import "./App.css";
-import './index.css'
-
-
-
 function App() {
   return (
     <div className="App">
-      <h1 className=" text-center font-bold text-4xl text-zinc-600">Holo world!</h1>
+      <h1 className=" text-center font-bold text-4xl text-zinc-600">
+        Holo world!
+      </h1>
     </div>
   );
 }
