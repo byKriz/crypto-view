3100c883c433d5003e7988a3ea4f88b97946b4b5
byKriz <cristianesquivelh@hotmail.com>
2022-08-18 13:15:55 -0400

    creando e implementando el navBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import {ThemeProvider} from './context/ThemeContext'
+import { NavBar } from "./containers/NavBar";
+import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
   return (
     <ThemeProvider>
-
+      <NavBar />
     </ThemeProvider>
   );
 }
