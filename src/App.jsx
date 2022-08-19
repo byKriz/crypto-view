f54e617f69a5c478b36101a809d793fc9c4bb809
byKriz <cristianesquivelh@hotmail.com>
2022-08-19 14:39:09 -0400

    creando el componente coinsearch

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,26 @@ import { Account } from "./routes/Account";
 import { Home } from "./routes/Home";
 import { SingIn } from "./routes/SingIn";
 import { SingUp } from "./routes/SingUp";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true';
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data)
+      console.log(response.data)
+    })
+  }, [url])
+
   return (
     <ThemeProvider>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home coins={coins}/>} />
         <Route path="/singin" element={<SingIn />} />
         <Route path="/singup" element={<SingUp />} />
         <Route path="/account" element={<Account />} />
---
+++ src/components/CoinSearch.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CoinSearch = () => {
+  return (
+    <div>CoinSearch</div>
+  )
+}
