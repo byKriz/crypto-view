a3df702f2526690577dd8f4316df3805a2f64c88
byKriz <cristianesquivelh@hotmail.com>
2022-08-19 14:11:02 -0400

    definiendo las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import { NavBar } from "./containers/NavBar";
 import { ThemeProvider } from "./context/ThemeContext";
+import { Account } from "./routes/Account";
+import { Home } from "./routes/Home";
+import { SingIn } from "./routes/SingIn";
+import { SingUp } from "./routes/SingUp";
 
 function App() {
   return (
     <ThemeProvider>
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/singin" element={<SingIn />} />
+        <Route path="/singup" element={<SingUp />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
     </ThemeProvider>
   );
 }
