cfa82e9b41cad676683dce8d08fca343ab0465e3
byKriz <cristianesquivelh@hotmail.com>
2022-08-20 17:57:25 -0400

    agregando la busqueda dinamica de cryptos

--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { CoinIteam } from "./CoinIteam";
 
 export const CoinSearch = ({ coins }) => {
-  // console.log(coins);
+  const [searchText, setSearchText] = useState("");
+
   return (
     <div>
       <div>
         <h1>Search Crypto</h1>
         <form>
-          <input type="text" placeholder="Search a coin" />
+          <input onChange={(e) => setSearchText(e.target.value)} type="text" placeholder="Search a coin" />
         </form>
       </div>
 
@@ -27,9 +28,20 @@ export const CoinSearch = ({ coins }) => {
           </tr>
         </thead>
         <tbody>
-          {coins.map((coin) => (
-            <CoinIteam coin={coin} key={coin.id} />
-          ))}
+          {/* filtro la busqueda y mapeo los resultados */}
+          {coins
+            .filter((value) => {
+              if (searchText === "") {
+                return value;
+              } else if (
+                value.name.toLowerCase().includes(searchText.toLowerCase()) || value.symbol.toLowerCase().includes(searchText.toLowerCase())
+              ) {
+                return value;
+              }
+            })
+            .map((coin) => (
+              <CoinIteam coin={coin} key={coin.id} />
+            ))}
         </tbody>
       </table>
     </div>
