ae4f39eebbe5fb6afc6d6f69f9bf989437a350f2
byKriz <cristianesquivelh@hotmail.com>
2022-08-20 17:44:52 -0400

    implementado coinItem

--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { AiOutlineStar } from "react-icons/ai";
+import { CoinIteam } from "./CoinIteam";
 
 export const CoinSearch = ({ coins }) => {
-  console.log(coins);
+  // console.log(coins);
   return (
     <div>
       <div>
@@ -28,26 +28,7 @@ export const CoinSearch = ({ coins }) => {
         </thead>
         <tbody>
           {coins.map((coin) => (
-            <tr key={coin.id}>
-              <td>
-                <AiOutlineStar />
-              </td>
-              <td>{coin.market_cap_rank}</td>
-              <td>
-                <div className="flex gap-2">
-                  <img src={coin.image} alt={coin.id} className=" h-5" />
-                  <p>{coin.name}</p>
-                </div>
-              </td>
-              <td className=" text-center">{coin.symbol}</td>
-              <td>{coin.current_price}$</td>
-              <td className={coin.price_change_percentage_24h >= 0 ? "text-center text-green-500" : "text-center text-red-500"}>
-                {coin.price_change_percentage_24h.toFixed(2)}%
-              </td>
-              <td>{coin.total_volume}</td>
-              <td>{coin.market_cap}</td>
-              <td>{coin.sparkline_in_7d.price}</td>
-            </tr>
+            <CoinIteam coin={coin} key={coin.id} />
           ))}
         </tbody>
       </table>
