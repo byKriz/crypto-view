20a82403ef1ac411322304d9144ba5ba6107102e
byKriz <cristianesquivelh@hotmail.com>
2022-08-29 14:16:14 -0400

    agregando la funcionalidad para crear usuarios

--- src/routes/SignUp.jsx
+++ src/routes/SignUp.jsx
@@ -1,17 +1,38 @@
-import React from "react";
+import { async } from "@firebase/util";
+import React, { useState } from "react";
 import { AiFillLock, AiOutlineMail } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 export const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const { signUp } = UserAuth();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await signUp(email, password);
+    } catch (e) {
+      setError(e.message);
+      console.log(e.message);
+    }
+  };
+
   return (
     <div>
       <div className="max-w-[400px] mx-auto min-h-[400px] px-4 py-15 mt-4">
         <h1 className="text-2xl font-bold">Sign Up</h1>
-        <form>
+        {error ? <p className="bg-red-300 p-3 my-2">{error}</p> : null}
+        <form onSubmit={handleSubmit}>
           <div className="my-4">
             <label>Email</label>
             <div className="my-2 w-full relative rounded-2xl shadow-xl">
               <input
+                onChange={(e) => setEmail(e.target.value)}
                 className="w-full p-2 bg-primary border border-input rounded-2xl"
                 type="email"
                 placeholder="Your email"
@@ -23,6 +44,7 @@ export const SignUp = () => {
             <label>Password</label>
             <div className="my-2 w-full relative rounded-2xl shadow-xl">
               <input
+                onChange={(p) => setPassword(p.target.value)}
                 className="w-full p-2 bg-primary border border-input rounded-2xl"
                 type="password"
                 placeholder="Your password"
@@ -31,7 +53,7 @@ export const SignUp = () => {
             </div>
           </div>
           <button className="w-full my-2 p-3 bg-button text-btnText rounded-2xl shadow-xl">
-            Sign in
+            Sign up
           </button>
         </form>
         <p className="my-4">
