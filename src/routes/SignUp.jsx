f32edfb19614e7ea0e4ddcfa879159fb5ee160e9
byKriz <cristianesquivelh@hotmail.com>
2022-08-29 17:18:24 -0400

    agregando el navigate a home

--- src/routes/SignUp.jsx
+++ src/routes/SignUp.jsx
@@ -16,6 +16,7 @@ export const SignUp = () => {
     setError("");
     try {
       await signUp(email, password);
+      navigate('/')
     } catch (e) {
       setError(e.message);
       console.log(e.message);
