a2b4386708b3f0e5a2c58ef8bce0db649082c814
byKriz <cristianesquivelh@hotmail.com>
2022-08-29 13:43:33 -0400

    ajustando el nombre de las variables de entorno

--- src/App.jsx
+++ src/App.jsx
@@ -9,33 +9,37 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { CoinPage } from "./pages/CoinPage";
 import { Footer } from "./containers/Footer";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   const [coins, setCoins] = useState([]);
 
-  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true';
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true";
 
   useEffect(() => {
     axios.get(url).then((response) => {
-      setCoins(response.data)
+      setCoins(response.data);
       // console.log(response.data)
-    })
-  }, [])
+    });
+  }, []);
 
   return (
     <ThemeProvider>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home coins={coins}/>} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/account" element={<Account />} />
-        {/* creando un link dinamico */}
-        <Route path="/coin/:coinId" element={<CoinPage />}>
-          <Route path=":coinId"/>
-        </Route>
-      </Routes>
-      <Footer />
+      <AuthContextProvider>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home coins={coins} />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/account" element={<Account />} />
+          {/* creando un link dinamico */}
+          <Route path="/coin/:coinId" element={<CoinPage />}>
+            <Route path=":coinId" />
+          </Route>
+        </Routes>
+        <Footer />
+      </AuthContextProvider>
     </ThemeProvider>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: REACT_APP_FIREBASE_API_KEY,
-  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: REACT_APP_FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
